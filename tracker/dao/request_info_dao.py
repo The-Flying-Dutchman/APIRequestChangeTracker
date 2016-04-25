ddc0e8bd9514fef511773ff27291889634972dc1
lirancohengwu <lirancohen@Lirans-Macbook-Pro.local>
2016-04-25 15:54:36 -0400

    DAO changes

--- tracker/dao/request_info_dao.py
+++ tracker/dao/request_info_dao.py
@@ -1,8 +1,57 @@
-class RequestInfoDao:
+from sql_connection import SqlConnection
+import pymysql.cursors
 
+class RequestInfoDao:
+    def __init__(self):
+        self.connection = SqlConnection.connection
+        
     def insert_request_info(self, request_info):
-        pass
+        if not request_info or not request_info.get("user_id") or not request_info.get("request_url") or not request_info.get("request_interval"):
+            return "Please pass in data"
+
+        with self.connection.cursor() as cursor:
+            sql = "INSERT INTO requests (request_user_id, request_url, request_interval) VALUES (%s, %s, %s)"
+            cursor.execute(sql, (request_info["user_id"], request_info["request_url"], request_info["request_interval"]))
+            self.connection.commit()
+            return "success"
 
     def select_request_info_by_requestid(self, requestid):
-        pass
+        with self.connection.cursor() as cursor:
+            sql = "SELECT * FROM requests WHERE request_id = %s"
+            cursor.execute(sql, requestid)
+            result = cursor.fetchall()
+            return result
+    
+    def select_requests(self):
+        with self.connection.cursor() as cursor:
+            sql = "SELECT * FROM requests"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+        return result
 
+    def select_request_info_by_userid(self, userid):
+        with self.connection.cursor() as cursor:
+            sql = "SELECT * FROM requests WHERE request_user_id = %s"
+            cursor.execute(sql, userid)
+            result = cursor.fetchall()
+            return result
+            
+    def update_request(self, request_info):
+        if not request_info or not request_info.get("request_id") or not request_info.get("request_url") or not request_info.get("request_interval"):
+            return "Please pass in data"
+
+        with self.connection.cursor() as cursor:
+                sql = "UPDATE requests SET request_url = %s, request_interval = %s WHERE request_id = %s"
+                cursor.execute(sql, (request_info["request_url"], request_info["request_interval"], request_info["request_id"]))
+                self.connection.commit()
+                return "success"
+    
+    def delete_request_by_requestid(self, requestid):
+        if requestid:
+            with self.connection.cursor() as cursor:
+                sql = "DELETE FROM requests WHERE request_id = %s"
+                cursor.execute(sql, requestid)
+                self.connection.commit()
+                return "success"
+        else:
+            return "Please pass in request id"
--- tracker/dao/user_dao.py
+++ tracker/dao/user_dao.py
@@ -34,7 +34,7 @@ class UserDao:
             sql = "INSERT INTO users (user_email, user_password) VALUES (%s, %s)"
             cursor.execute(sql, (user["email"], user["password"]))
             self.connection.commit()
-            return "User inserted"
+            return "success"
 
     def update_user(self, user):
         if not user or not user.get("user_id"):
@@ -44,7 +44,7 @@ class UserDao:
                 sql = "UPDATE users SET user_email = %s, user_password = %s WHERE user_id = %s"
                 cursor.execute(sql, (user["email"], user["password"], user["user_id"]))
                 self.connection.commit()
-                return "User updated"
+                return "success"
 
     def delete_user_by_userid(self, user_id):
         if user_id:
@@ -52,9 +52,9 @@ class UserDao:
                 sql = "DELETE FROM users WHERE user_id = %s"
                 cursor.execute(sql, user_id)
                 self.connection.commit()
-                return "User deleted"
+                return "success"
         else:
             return "Please pass in user id"
 
-user_dao = UserDao()
-print user_dao.select_user_by_useremail("hello")
+#user_dao = UserDao()
+#print user_dao.select_user_by_useremail("hello")
