e2b5ba11ad73f088475a6347e0691212086739aa
Haoyang <shepherdtien@gmail.com>
2016-04-28 17:30:35 -0400

    Finished dynamic data in HTML using jQuery MVC structure!

--- tracker/static/scripts/user.js
+++ tracker/static/scripts/user.js
@@ -1,12 +1,65 @@
 ﻿$(function(){
-	setTimeout(function(){
-		$('.logo').addClass("zoom");
-	},4000);
-	setTimeout(function(){
-		$('.current-request-section').addClass("bounceInFromBottom");
-		$('.current-request-section').show();
-	},2500);
-	$(".edit-icon").click(function(){
+	//Model
+	var Model = (function() {
+		var URL = "http://localhost:8080/jQuery_MVC/test.json";
+		return {
+			getRequests : function() {
+				return $.ajax({
+					type : "GET",
+					url : URL,
+					dataType : "json",
+					contentType : "application/json",
+					success : function(text) {
+						
+					},
+					error: function(textStatus, errorThrown){
+						alert("Loading data error!");
+					}
+				});
+			}
+		};
+	}());
+
+	//View
+	var View = (function() {
+		var template = $("#request-template").html();
+
+		var applyTemplate = function(template, data) {
+			return template
+				.replace(/\${requestURL}/g, data.URL)
+				.replace(/\${timeInterval}/g, data.Time);
+		};
+
+		var renderRequests = function(results) {
+			return results.map(function(data) {
+				return applyTemplate(template, data);
+			}).join("");
+		};
+
+		return {
+			displayRequests : function(results){
+				var rendering = renderRequests(results);
+				$(".current-request-section-list").html(rendering);
+			}
+		};
+	}());
+
+	//Controller
+	var Controller = (function() {
+		return {
+			loadAndDisplayRequests : function() {
+				var requests = Model.getRequests();//Load
+				requests.done(function(results) {
+					View.displayRequests(results);   //Display
+				});
+			}
+		};
+	}());
+	
+	
+	
+	//Add Click Events (Still works for dynamically created HTML elements)
+	$(document).on("click", ".edit-icon", function(){
 		var parent = $(this).parent();
 		var original_url = parent.children("p:nth-of-type(1)").children("span").text();
 		var original_time_interval = parent.children("p:nth-of-type(2)").children("span").text();
@@ -20,7 +73,7 @@
 			parent.siblings(".edit").addClass("flipIn");
 		},500);
 	});
-	$(".cancel-icon").click(function(){
+	$(document).on("click", ".cancel-icon",function(){
 		var parent = $(this).parent();
 		parent.addClass("flipOut");
 		setTimeout(function(){
@@ -30,4 +83,18 @@
 			parent.siblings(".display").addClass("flipIn");
 		},500);
 	});
+	
+	//Animation
+	setTimeout(function(){
+		$('.logo').addClass("zoom");
+	},4000);
+	setTimeout(function(){
+		$('.current-request-section').addClass("bounceInFromBottom");
+		$('.current-request-section').show();
+	},2500);
+	
+	//Init controller
+	$(document).ready(function () {
+		Controller.loadAndDisplayRequests();
+    });
 });
