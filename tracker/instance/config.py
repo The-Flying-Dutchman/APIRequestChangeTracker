6f56f85efeee9e78adf87f237a2ba97bbb8be072
lirancohengwu <lirancohen@Lirans-Macbook-Pro.local>
2016-04-28 12:31:11 -0400

    requests

---
+++ tracker/instance/config.py
@@ -0,0 +1,12 @@
+# instance/config.py
+# This file configure variables could not expose to public
+
+DEBUG = True
+
+# mysql address
+DB_HOST = "162.243.65.63"
+DB_USERNAME = "root"
+DB_PASSWORD = "weisql"
+
+# Secret key for signing cookies
+SECRET_KEY = "secret"
