6aa159a1bfd749f036b0188c591fd747a1df725c
benmhess <benmhess@yahoo.com>
2016-04-28 14:18:06 -0400

    update

--- tracker/static/scripts/user.js
+++ tracker/static/scripts/user.js
@@ -1,4 +1,23 @@
 ﻿$(function(){
+﻿	$.ajax{
+		var user_id = '<%= Session["user_id"] %>';
+		url: "/list",
+		data: {"user_id": 1},
+		success: function(data){
+			var array = data;
+			for(var i=0; i<=array.length; i++){
+				var content = '<div class="current-request-section">
+				<p>Request URL:<span>'+array[i]["request_url"]+'</span></p>
+				<p>Time interval:<span>'+aray[i]["request_interval"]+'</span></p>
+				<a class="edit-icon"><img src="images/edit.png" /></a>
+				<a class="delete-icon"><img src="images/delete.png" /></a>
+				</div>';
+				$(".current-request-section-list").append(content);
+			}
+		}, error: function(error) {
+			console.log("error");
+		}
+	}
 	setTimeout(function(){
 		$('.logo').addClass("zoom");
 	},4000);
