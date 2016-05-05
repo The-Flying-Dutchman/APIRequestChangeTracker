e319ffae7de1042b2849dd452b5a6b7452d63eaf
wei <liwei.day@gmail.com>
2016-05-05 02:37:45 -0400

    update

---
+++ tests/tracker_tests.py
@@ -0,0 +1,39 @@
+import os
+import tracker
+import unittest
+import tempfile
+
+class FlaskrTestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_login(self):
+        rv = self.login('wei', 'wei')
+        print rv
+        # assert 'success' in rv.data
+
+    def test_list(self):
+        self.login('wei', 'wei')
+        rv = self.app.get('/get_request_lists',
+                          data = {"user_id":6})
+        assert 'request_id' in rv.data
+
+    def test_detail(self):
+        pass
+
+    def login(self, username, password):
+        return self.app.post('/login', data=dict(
+            email=username,
+            password=password
+        ), follow_redirects=True)
+
+    def logout(self):
+        return self.app.get('/logout', follow_redirects=True)
+
+    def tearDown(self):
+        pass
+
+
+
+if __name__ == '__main__':
+    unittest.main()
--- tracker/static/scripts/index.js
+++ tracker/static/scripts/index.js
@@ -1,43 +1,62 @@
-﻿$(function(){
-	setTimeout(function(){
-		$('.logo').addClass("zoom");
-	},3000);
-	$(".register-section .login-tag").click(function(){
-		$(".register-section").addClass("rotateOut");
-		setTimeout(function(){
-			$(".register-section").removeClass("rotateOut");
-			$(".register-section").hide();
-			$(".login-section").show();
-			$(".login-section").addClass("rotateIn");
-		},400);
-	});
-	$(".login-section .register-tag").click(function(){
-		$(".login-section").addClass("rotateOut");
-		setTimeout(function(){
-			$(".login-section").removeClass("rotateOut");
-			$(".login-section").hide();
-			$(".register-section").show();
-			$(".register-section").addClass("rotateIn");
-		},400);
-	});
+﻿$(function () {
+    setTimeout(function () {
+        $('.logo').addClass("zoom");
+    }, 3000);
+    $(".register-section .login-tag").click(function () {
+        $(".register-section").addClass("rotateOut");
+        setTimeout(function () {
+            $(".register-section").removeClass("rotateOut");
+            $(".register-section").hide();
+            $(".login-section").show();
+            $(".login-section").addClass("rotateIn");
+        }, 400);
+    });
+    $(".login-section .register-tag").click(function () {
+        $(".login-section").addClass("rotateOut");
+        setTimeout(function () {
+            $(".login-section").removeClass("rotateOut");
+            $(".login-section").hide();
+            $(".register-section").show();
+            $(".register-section").addClass("rotateIn");
+        }, 400);
+    });
 
 });
 
-function pwdVerify(){
-	var pwd1 = $("#pass").val();
-	var pwd2 = $("#ver").val();
-	if($('#registerForm')[0].checkValidity()){
-		if( pwd1 != pwd2){
-			$("#indicator").show();
-			$("#indicator").addClass("showFromTop");
-			setTimeout(function(){
-				$("#indicator").removeClass("showFromTop");
-				$("#indicator").fadeOut();
-			},2000);
-			return false;
-		}else{
-			return true;
-		}
-	}else
-		return true;
+function pwdVerify() {
+    var pwd1 = $("#pass").val();
+    var pwd2 = $("#ver").val();
+    if ($('#registerForm')[0].checkValidity()) {
+        if (pwd1 != pwd2) {
+            $("#indicator").show();
+            $("#indicator").addClass("showFromTop");
+            setTimeout(function () {
+                $("#indicator").removeClass("showFromTop");
+                $("#indicator").fadeOut();
+            }, 2000);
+            return false;
+        } else {
+            $.ajax({
+                type: "POST",
+                url: "/register",
+                data: $("#registerForm").serialize(),
+                success: function (data) {
+                    var data = jQuery.parseJSON(data)
+                    if (data.message == "success") {
+                        location.href = "/list";
+                    } else {
+                        location.href = "/";
+                    }
+                },
+
+                error: function (data) {
+                    location.href = "/"
+                }
+            });
+            return false;
+        }
+    } else {
+        return true;
+    }
+
 }
